375e9bbf79c6a5c3dda1f890f5b57a49c8cbfae9
Florian Klampfer <mail@qwtel.com>
2022-04-01 11:15:02 +0700

    Fix test

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,8 @@
 import './fixes';
 import { jest } from '@jest/globals'
 
+import { RequestCookieStore } from '@worker-tools/request-cookie-store';
 import { SignedCookieStore } from '../index.js'
-import { RequestCookieStore } from '../../middleware/node_modules/@worker-tools/request-cookie-store';
 
 test('exists', () => {
   expect(SignedCookieStore).toBeDefined
